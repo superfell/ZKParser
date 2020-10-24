9d8d8067eb74efa5247360278d2319d7e617db2a
Simon Fell <fellforce@gmail.com>
2020-10-23 20:05:24 -0700

    Initial commit

---
+++ ZKParser/ZKParser.h
@@ -0,0 +1,13 @@
+//
+//  ZKParser.h
+//  ZKParser
+//
+//  Created by Simon Fell on 10/23/20.
+//  Copyright © 2020 Simon Fell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZKParser : NSObject
+
+@end
---
+++ ZKParser/ZKParser.m
@@ -0,0 +1,13 @@
+//
+//  ZKParser.m
+//  ZKParser
+//
+//  Created by Simon Fell on 10/23/20.
+//  Copyright © 2020 Simon Fell. All rights reserved.
+//
+
+#import "ZKParser.h"
+
+@implementation ZKParser
+
+@end
